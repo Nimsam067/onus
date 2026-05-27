fc1c2f4118388cfcd58027d46b96b11d2df3e748
Nimsam067 <aadikothari2006@gmail.com>
2026-05-27 12:35:06 +0800

    Removing styling from content dashboard

--- onus-react/src/components/content_dashboard.jsx
+++ onus-react/src/components/content_dashboard.jsx
@@ -1,15 +1,7 @@
 function ContentDashboard() {
     return (
         <div className="content-dashboard">     
-            <h3
-            style={{
-              color: "black",
-              textAlign: "center",
-              padding: "10px",
-              fontSize: "20px",
-              fontFamily: "Poppins",
-            }}
-            >
+            <h3>
             Contribution Dashboard
             </h3>
             <p>Current Streak: 7 days</p>

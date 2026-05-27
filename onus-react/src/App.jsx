a153f9052d3ee87d1eda92c6afd87a2714738cbb
Nimsam067 <aadikothari2006@gmail.com>
2026-05-27 11:32:11 +0800

    feat: enhance layout with app container and dashboard grid styles

--- onus-react/src/App.jsx
+++ onus-react/src/App.jsx
@@ -1,11 +1,26 @@
 import TaskDashboard from "./components/tasks_dashboard";
 import ContentDashboard from "./components/content_dashboard";
+import "./App.css";
+
 
 function App() {
   return (
+    /*
     <div>
       <TaskDashboard />
       <ContentDashboard />
+    </div>*/
+
+    <div className="app-container">
+      <div className="dashboard-grid">
+        <div className="card">
+          <TaskDashboard />
+        </div>
+
+        <div className="card">
+          <ContentDashboard />
+        </div>
+      </div>
     </div>
   );
 }
